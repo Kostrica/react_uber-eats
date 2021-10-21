0b26ac9e7492e7d11a2a4893ae50bd2611924383
Kostrica <romiorw@ukr.net>
2021-10-21 19:58:21 +0300

    Updates

---
+++ precache-manifest.a0b9ff14bc964061cdaee119791b07fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca02508c52e8a8f083a4c6f632ede8c",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "d7ab86f93f64e46237da",
+    "url": "/react_uber-eats/static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "66d6bc34c3804480e9c0",
+    "url": "/react_uber-eats/static/css/main.fb44627c.chunk.css"
+  },
+  {
+    "revision": "d7ab86f93f64e46237da",
+    "url": "/react_uber-eats/static/js/2.6787b654.chunk.js"
+  },
+  {
+    "revision": "66d6bc34c3804480e9c0",
+    "url": "/react_uber-eats/static/js/main.5ae02e09.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  },
+  {
+    "revision": "903ab651c3007be7a2006306ce088b48",
+    "url": "/react_uber-eats/static/media/Logo.903ab651.svg"
+  },
+  {
+    "revision": "53aa94d33e59dbe2d35cab07f63feca3",
+    "url": "/react_uber-eats/static/media/close-icon.53aa94d3.svg"
+  },
+  {
+    "revision": "4757b73b34417927eead99945d64c8c1",
+    "url": "/react_uber-eats/static/media/footer-logo.4757b73b.svg"
+  }
+]);
